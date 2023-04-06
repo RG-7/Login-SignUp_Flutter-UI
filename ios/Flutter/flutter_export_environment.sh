3ce5814980292f17961ed410415d45e413e3fd04
FlutterBaba <72684684+FlutterBaba@users.noreply.github.com>
2023-04-06 23:02:43 +0500

    done

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/mapmac/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mapmac/Desktop/flutter_login_signup"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/mapmac/Desktop/flutter_login_signup/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/mapmac/Desktop/flutter_login_signup/.dart_tool/package_config.json"
